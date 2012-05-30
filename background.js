5e4a77e7be5d9498b5ded52418cbacbaa32c8849
Franklin <fr@nkl.in>
2012-05-31 00:36:56 +0200

    Background: cleanup

--- background.js
+++ background.js
@@ -13,15 +13,10 @@ chrome.extension.onRequest.addListener( function( request, sender, response ) {
 				}
 			}
 			
-			
-			// update & display icon
-			chrome.pageAction.setTitle({
-				tabId:	sender.tab.id,
-				title:	tabs[ sender.tab.id ].amount == 1 ? 'Found a Twitter user' : 'Found '+ tabs[ sender.tab.id ].amount +' Twitter users'
-			});
 			tabs[ 't'+ sender.tab.id ].users[ request.user.username ] = request.user;
 			tabs[ 't'+ sender.tab.id ].amount++;
 			
+			// display icon
 			chrome.pageAction.show( sender.tab.id );
 			
 			// all good
