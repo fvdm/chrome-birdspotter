a8e7a3f2490db7c1efa03c9c4197cfe237a3cc0e
Franklin <fr@nkl.in>
2012-05-30 22:26:56 +0200

    Content script
    This parses Twitter links out of the document body using native functions instead of DOM iteration.

---
+++ twitter_detect.js
@@ -0,0 +1,19 @@
+if( document.links.length >= 1 ) {
+	for( var l in document.links ) {
+		var link = document.links[l];
+		if( link.href ) {
+			link.href.replace( /^https?:\/\/twitter\.com\/(#!\/)?([a-z0-9_]+)$/, function( str, hash, user ) {
+				chrome.extension.sendRequest({
+					action:	'twitterUser',
+					user:	{
+						username:	user,
+						title:		link.title ? link.title : false,
+						text:		link.innerText && link.innerText != '' ? link.innerText : false,
+						level:		'page'
+					},
+					host:	document.location.host
+				});
+			});
+		}
+	}
+}
