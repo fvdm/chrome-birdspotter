6fd975f51d4257436abac33558e54469d7e2065f
Franklin <fr@nkl.in>
2012-06-04 19:45:15 +0200

    Renamed twitter_detect.js to bird_spotter.js

--- bird_spotter.js
+++ bird_spotter.js
@@ -1,6 +1,6 @@
 if( document.location.host != 'twitter.com' && document.links.length >= 1 ) {
 	for( var l in document.links ) {
-		var link = document.links[l];
+		var link = document.links[l]
 		if( link.href ) {
 			link.href.replace( /^https?:\/\/twitter\.com\/(#!\/)?([a-z0-9_]+)$/, function( str, hash, user ) {
 				chrome.extension.sendRequest({
@@ -11,8 +11,8 @@ if( document.location.host != 'twitter.com' && document.links.length >= 1 ) {
 						text:		link.innerText && link.innerText != '' ? link.innerText : false
 					},
 					href:	document.location.href
-				});
-			});
+				})
+			})
 		}
 	}
 }
