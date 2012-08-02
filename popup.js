19fa4d47c8cca073381251cfcb718303218571da
Franklin <fr@nkl.in>
2012-08-02 22:15:09 +0200

    Added offline mode

--- popup.js
+++ popup.js
@@ -17,56 +17,73 @@ chrome.tabs.query(
 			},
 			function( response ) {
 				
-				// let's build the popup
-				document.getElementById('wrap').innerHTML = '<ul id="list"></ul>'
-				
-				for( var u in response.users ) {
+				if( navigator.onLine ) {
 					
-					// !init
-					var li = document.createElement('li'),
-					avatarDiv = document.createElement('div'),
-					avatarLink = document.createElement('a'),
-					avatarImage = document.createElement('img'),
-					nameDiv = document.createElement('div'),
-					screennameDiv = document.createElement('div'),
-					screennameLink = document.createElement('a'),
-					buttonDiv = document.createElement('div'),
-					buttonIframe = document.createElement('iframe')
+					// !Browser online
+					document.getElementById('wrap').innerHTML = '<ul id="list"></ul>'
+					for( var u in response.users ) {
+						
+						var user = response.users[u].username,
+						li = document.createElement('li'),
+						avatarDiv = document.createElement('div'),
+						avatarLink = document.createElement('a'),
+						avatarImage = document.createElement('img'),
+						nameDiv = document.createElement('div'),
+						screennameDiv = document.createElement('div'),
+						screennameLink = document.createElement('a'),
+						buttonDiv = document.createElement('div'),
+						buttonIframe = document.createElement('iframe')
+						
+						avatarImage.src = 'https://api.twitter.com/1/users/profile_image?screen_name='+ user +'&size=normal&dnt=true'
+						avatarLink.target = '_blank'
+						avatarLink.href = 'https://twitter.com/'+ user
+						avatarLink.appendChild( avatarImage )
+						avatarDiv.className = 'avatar'
+						avatarDiv.appendChild( avatarLink )
+											
+						screennameLink.target = '_blank'
+						screennameLink.href = 'https://twitter.com/'+ user
+						screennameLink.innerHTML = user
+						screennameDiv.className = 'screenname'
+						screennameDiv.appendChild( screennameLink )
+						
+						buttonIframe.setAttribute( 'allowtransparency', true )
+						buttonIframe.setAttribute( 'frameBorder', 0 )
+						buttonIframe.setAttribute( 'scrolling', 'no' )
+						buttonIframe.setAttribute( 'src', 'https://platform.twitter.com/widgets/follow_button.html?screen_name='+ user +'&lang=en&dnt=true&show_count=true&show_screen_name=false' )
+						buttonDiv.className = 'button'
+						buttonDiv.appendChild( buttonIframe )
+						
+						nameDiv.className = 'name'
+						nameDiv.appendChild( screennameDiv )
+						nameDiv.appendChild( buttonDiv )
+						
+						li.className = 'itemOnline'
+						li.appendChild( avatarDiv )
+						li.appendChild( nameDiv )
+						document.getElementById('list').appendChild( li )
+						
+					}
 					
-					// !avatar
-					avatarImage.src = 'https://api.twitter.com/1/users/profile_image?screen_name='+ response.users[u].username +'&size=normal&dnt=true'
-					avatarLink.target = '_blank'
-					avatarLink.href = 'https://twitter.com/'+ response.users[u].username
-					avatarLink.appendChild( avatarImage )
-					avatarDiv.className = 'avatar'
-					avatarDiv.appendChild( avatarLink )
-										
-					// !screenname
-					screennameLink.target = '_blank'
-					screennameLink.href = 'https://twitter.com/'+ response.users[u].username
-					screennameLink.innerHTML = response.users[u].username
-					screennameDiv.className = 'screenname'
-					screennameDiv.appendChild( screennameLink )
+				} else {
 					
-					// !button
-					buttonIframe.setAttribute( 'allowtransparency', true )
-					buttonIframe.setAttribute( 'frameBorder', 0 )
-					buttonIframe.setAttribute( 'scrolling', 'no' )
-					buttonIframe.setAttribute( 'src', 'https://platform.twitter.com/widgets/follow_button.html?screen_name='+ response.users[u].username +'&lang=en&dnt=true&show_count=true&show_screen_name=false' )
-					buttonIframe.style.width = '240px'
-					buttonIframe.style.height = '20px'
-					buttonDiv.className = 'button'
-					buttonDiv.appendChild( buttonIframe )
-					
-					// !name
-					nameDiv.className = 'name'
-					nameDiv.appendChild( screennameDiv )
-					nameDiv.appendChild( buttonDiv )
-					
-					// !list item
-					li.appendChild( avatarDiv )
-					li.appendChild( nameDiv )
-					document.getElementById('list').appendChild( li )
+					// !Browser offline
+					document.getElementById('wrap').innerHTML = '<div id="offline" title="User avatars and follow buttons are not visible because your browser is offline.">browser offline</div><ul id="list"></ul>'
+					for( var u in response.users ) {
+						
+						var user = response.users[u].username,
+						li = document.createElement('li'),
+						screennameLink = document.createElement('a')
+						
+						screennameLink.target = '_blank'
+						screennameLink.href = 'https://twitter.com/'+ user
+						screennameLink.className = 'screenname'
+						screennameLink.innerHTML = user
+						
+						li.className = 'itemOffline'
+						li.appendChild( screennameLink )
+						document.getElementById('list').appendChild( li )
+					}
 					
 				}
 				
