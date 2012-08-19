fc96207236eb8c4ef4e401aee9955bad8308c9de
Franklin van de Meent <fr@nkl.in>
2012-08-20 00:48:44 +0200

    Detect `&via=username` in share links

--- bird_spotter.js
+++ bird_spotter.js
@@ -11,7 +11,10 @@ if( document.location.host != 'twitter.com' && document.links.length >= 1 ) {
 					foundUser( username )
 				}
 			})
-			link.href.replace( /^https?:\/\/(www\.)?twitter\.com\/intent\/(user|tweet)\?.*screen_name=([a-z0-9_]+)&?.*$/i, function( str, www, mid, username ) {
+			link.href.replace( /^https?:\/\/(www\.)?twitter\.com\/intent\/(user|tweet)\?.*\bscreen_name=([a-z0-9_]+)&?.*$/i, function( str, www, mid, username ) {
+				foundUser( username )
+			})
+			link.href.replace( /^https?:\/\/(www\.)?twitter\.com\/share\?.*\bvia=([a-z0-9_]+)&?.*$/i, function( str, www, username ) {
 				foundUser( username )
 			})
 		}
