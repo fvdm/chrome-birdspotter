ade717716b1a29c12039452587e9db04f26b99f8
Franklin van de Meent <fvdm@users.noreply.github.com>
2016-04-28 04:44:03 +0200

    Background: removed all remote resources

--- background.js
+++ background.js
@@ -2,11 +2,7 @@
 var tabs = {};
 var prefs = {};
 var defaults = {
-  avatars: true,
   onTwitter: false,
-  realnames: false,
-  apilookup: true,
-  dnt: true,
   click: 'intent', // intent, tab, custom, nothing
   customURL: ''
 };
@@ -34,16 +30,6 @@ chrome.extension.onRequest.addListener (function (request, sender, response) {
   }
 
   switch (request.action) {
-
-    // !API limit
-    case 'apiLimit':
-      apiLimit (function (res) {
-        response ({
-          limit: res,
-          status: 'ok'
-        });
-      });
-      break;
 
     // !Options - GET
     case 'getOptions':
@@ -73,14 +59,6 @@ chrome.extension.onRequest.addListener (function (request, sender, response) {
 
         // !Display icon
         chrome.pageAction.show (tabId);
-
-        // collect usernames for mass lookup (save API calls)
-        var all_usernames = Object.keys (tabs ['t'+ tabId] .users).join (',');
-
-        // fetch em, and replace when it is found
-        fetch_bird (all_usernames, function (bird) {
-          tabs ['t'+ tabId] .users [bird.screen_name.toLowerCase ()] = bird;
-        });
       }
 
       response ({ status: 'ok' });
@@ -91,11 +69,7 @@ chrome.extension.onRequest.addListener (function (request, sender, response) {
 
       // !add to users
       var username = request.user.toLowerCase ();
-      if( tabs ['t'+ tabId] .users [username] === undefined) {
-        tabs ['t'+ tabId] .users [username] = {
-          screen_name: username
-        };
-      }
+      tabs ['t'+ tabId] .users [username] = username;
 
       response ({ status: 'ok' });
       break
@@ -116,88 +90,6 @@ chrome.tabs.onRemoved.addListener (function (tabId, removeInfo) {
   }
 });
 
-// !Get API limit
-function apiLimit (cb) {
-  var xhr = new XMLHttpRequest ();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var data = xhr.responseText;
-      if (data.substr (0,1) === '{' && data.substr (data.length -1, 1) === '}') {
-        data = JSON.parse (data);
-        if (data.hourly_limit !== undefined) {
-          cb (data);
-        }
-      }
-    }
-  };
-  xhr.open ('GET', 'https://twitter.com/account/rate_limit_status.json' + (prefs.dnt ? '?dnt=true' : ''), true);
-  xhr.send ();
-}
-
-// !Get user from Twitter API
-function fetch_bird (usernames, cb) {
-  if (navigator.onLine && prefs.apilookup) {
-    apiLimit (function (limit) {
-      if (limit.remaining_hits >= 1) {
-
-        // !Fetcher: Browser online
-        // commented lines are not used atm, saves memory
-        http_request (usernames, function (bird) {
-          cb ({
-            screen_name: bird.screen_name,
-            id_str: bird.id_str,
-            name: bird.name,
-            description: bird.description,
-            followers_count: bird.followers_count,
-            followers_count_human: human_number (bird.followers_count),
-            profile_image_url_https: bird.profile_image_url_https,
-    //        following: bird.following,
-    //        follow_request_sent: bird.follow_request_sent
-          });
-        });
-      } else {
-        // !Fetcher: API Limit reached
-        usernames = usernames.split (',');
-        for (var u in usernames) {
-          cb ({ screen_name: usernames [u] });
-        }
-      }
-    });
-  } else {
-    // !Fetcher: Browser offline
-    usernames = usernames.split (',');
-    for (var u in usernames) {
-      cb ({ screen_name: usernames [u] });
-    }
-  }
-}
-
-// !HTTP REQUEST
-function http_request (usernames, cb) {
-  var xhr = new XMLHttpRequest ();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var data = xhr.responseText.trim ();
-      if (data.match (/^(\{.*\}|\[.*\])$/)) {
-        data = JSON.parse (data);
-        if (data && data [0] && data [0] .screen_name) {
-          for (var t in data) {
-            cb (data [t]);
-          }
-        }
-      }
-    }
-  };
-
-  xhr.open ('GET', 'https://api.twitter.com/1.1/users/lookup.json?screen_name='+ usernames +'&include_entities=false', true);
-
-  if (prefs.dnt) {
-    xhr.setRequestHeader ('DNT', '1');
-  }
-
-  xhr.send ();
-}
-
 // !Load options
 function loadOptions (cb) {
   chrome.storage.sync.get ('options', function (res) {
@@ -214,15 +106,3 @@ chrome.storage.onChanged.addListener (function (ch, ns) {
 
 // first load
 loadOptions ();
-
-// !Human numbers
-function human_number (number) {
-  var units = ['', 'K', 'M', 'G', 'T'];
-  var num = number;
-  var i = 0;
-  while( num >= 10000 ) {
-    num = num / 1000;
-    i++;
-  }
-  return Math.floor (num) + units [i];
-}
